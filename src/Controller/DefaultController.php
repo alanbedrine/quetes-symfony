0ff6d8924e75d020157cc1be30373f2507823691
alan.bedrine <alan.bedrine@gmail.com>
2022-12-01 10:19:36 +0100

    Add DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    #[Route('/home', name: 'home2')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
         ]);
     }
 }
